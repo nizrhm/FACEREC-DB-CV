c6b54c4615fc342b3a50279ce6a7ec9df705b424
Nizaul Rahman <nizaulr@outlook.com>
2023-09-10 17:30:45 +0530

    First Commit, Without Images

---
+++ cam.py
@@ -0,0 +1,11 @@
+#this code will take input, its just for understanding
+import cv2
+
+cap = cv2.VideoCapture(1)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Face Attendace", img)
+    cv2.waitKey(1)
